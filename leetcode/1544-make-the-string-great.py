1d6c7e97c2b89d6bcc7fb57a436cc3b32b3a700d
ThatNerdSquared <72814106+ThatNerdSquared@users.noreply.github.com>
2024-04-07 16:04:30 -0700

    feat: non-recursive leetcode 1544 solution

--- leetcode/1544-make-the-string-great.py
+++ leetcode/1544-make-the-string-great.py
@@ -1,28 +1,13 @@
 class Solution:
-    def makeGood(self, s: str, prev_res: str = "") -> str:
-        # handle base case first
-        if len(s) < 2:
-            return prev_res + s
+    def makeGood(self, s: str) -> str:
+        res = []
+        queue = list(s)
+        for _ in range(len(queue)):
+            current = queue.pop(0)
 
-        # split important values out for convenience
-        current = s[0]
-        next = s[1]
-        is_same_char = current.lower() == next.lower()
-        not_same_case = current.isupper() != next.isupper()
+            if res and current.lower() == res[-1].lower() and current.isupper() != res[-1].isupper():
+                res.pop()
+            else:
+                res.append(current)
 
-        # natural recursion
-        if is_same_char and not_same_case:
-            if prev_res == "":
-                return self.makeGood(
-                    s[2:],
-                    prev_res
-                )
-            return self.makeGood(
-                prev_res[-1] + s[2:],
-                prev_res[:-1]
-            )
-        else:
-            return self.makeGood(
-                s[1:],
-                prev_res + current
-            )
+        return ''.join(res)
